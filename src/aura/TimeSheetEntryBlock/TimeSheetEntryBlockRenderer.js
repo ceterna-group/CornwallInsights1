8f4ce9953f98eb21d8e19ec7b902c0c856553d00
Ronan Williams <ronan@ceterna.com>
2019-08-11 19:55:20 +0100

    Timesheet changes

--- src/aura/TimeSheetEntryBlock/TimeSheetEntryBlockRenderer.js
+++ src/aura/TimeSheetEntryBlock/TimeSheetEntryBlockRenderer.js
@@ -7,7 +7,7 @@
     afterRender : function($C,$H) {
         var listInputCmp    = $C.find("codelistInput");
         var listInput       = listInputCmp.getElement();
-        // listInput.setAttribute("list", "codelist");
+        // listInput.setAttribute("list", "mylist");
         listInput.setAttribute("list", $C.get('v.recordId'));
         //
         // console.log('superAfterRender fired');
